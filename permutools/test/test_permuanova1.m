85e933c729c74e313aca2408e78e15a679b70382
Mick Crosse <mickcrosse@gmail.com>
2024-01-15 21:33:17 +0000

    Update test_permuanova1.m

--- permutools/test/test_permuanova1.m
+++ permutools/test/test_permuanova1.m
@@ -11,21 +11,21 @@ x = randn(30,5);
 group = 1:5;
 
 % Perform one-way ANOVA
-[p1,Table1,stats1] = anova1(x,group,'off');
+[p1,tbl1,stats1] = anova1(x,group,'off');
 
 % Perform permutation one-way ANOVA
-[~,p2,~,stats2,Table2] = permuanova1(x,group);
+[~,p2,~,stats2,tbl2] = permuanova1(x,group);
 
 % Assert that results are the same or similar
 assert(abs(p1-p2)<0.005)
-assert(round(Table1{6},10)==round(Table2{6},10))
-assert(round(Table1{7},10)==round(Table2{7},10))
-assert(round(Table1{8},10)==round(Table2{8},10))
-assert(Table1{10}==Table2{10})
-assert(Table1{11}==Table2{11})
-assert(Table1{12}==Table2{12})
-assert(round(Table1{14},10)==round(Table2{14},10))
-assert(round(Table1{15},10)==round(Table2{15},10))
+assert(round(tbl1{6},10)==round(tbl2{6},10))
+assert(round(tbl1{7},10)==round(tbl2{7},10))
+assert(round(tbl1{8},10)==round(tbl2{8},10))
+assert(tbl1{10}==tbl2{10})
+assert(tbl1{11}==tbl2{11})
+assert(tbl1{12}==tbl2{12})
+assert(round(tbl1{14},10)==round(tbl2{14},10))
+assert(round(tbl1{15},10)==round(tbl2{15},10))
 assert(any(ismember(stats1.gnames,stats2.gnames)))
 assert(any(stats1.n==stats2.n))
 assert(any(stats1.means==stats2.means))
