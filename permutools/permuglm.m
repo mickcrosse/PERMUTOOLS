0599dab1fca7ec053249deda918b69ca2428058b
Mick Crosse <mickcrosse@gmail.com>
2026-08-20 00:13:51 +0100

    Added descriptive stats

--- permutools/permuglm.m
+++ permutools/permuglm.m
@@ -94,14 +94,13 @@ end
 
 % Transform raw data to rank orders if specified
 switch arg.type
-    case 'freedmanlane'
-    case 'manly'
+    case {'freedmanlane', 'manly'}
     case 'rank'
         for k = 1:size(y,2)
             y(:,k) = tiedrank(y(:,k));
         end
     otherwise
-        error('The TYPE parameter value must be FREEDMANLANE, or RANK.')
+        error('The TYPE parameter value must be FREEDMANLANE, MANLY, or RANK.')
 end
 
 % Append intercept if requested and missing
@@ -110,130 +109,143 @@ if arg.intercept && ~any(all(x==1,1))
 end
 
 % Get data dimensions
-[nobs,pnum] = size(x);
-v = size(y,2);
-df = nobs-pnum;
+[nobs,npred] = size(x);
+nvar = size(y,2);
 
 % Compute observed statistics
-b = zeros(pnum,v);
-se = zeros(pnum,v);
-mu_full = zeros(nobs,v);
-for k = 1:v
-    [b(:,k),W_full,eta,~] = ptlocalfitglm(x,y(:,k),arg.distribution);
-    XW = x.*W_full;
-    inv_fisher = pinv(XW'*x);
-    se(:,k) = sqrt(diag(inv_fisher));
-    if strcmpi(arg.distribution,'binomial')
-        mu_full(:,k) = 1./(1+exp(-eta));
-    elseif strcmpi(arg.distribution,'poisson')
-        mu_full(:,k) = exp(eta);
-    else
-        mu_full(:,k) = eta;
+b = zeros(npred,nvar);
+se = zeros(npred,nvar);
+mu = zeros(nobs,nvar);
+dispersion = zeros(1,nvar);
+df = nobs-npred;
+for k = 1:nvar
+    [b(:,k),Wfull,eta] = ptlocalfitglm(x,y(:,k),arg.distribution);
+    XW = x.*Wfull;
+    invfisher = pinv(XW'*x);
+    switch arg.distribution
+        case 'normal'
+            res = y(:,k)-eta;
+            dispersion(k) = sum(res.^2)/df;
+        otherwise
+            dispersion(k) = 1;
+    end
+    se(:,k) = sqrt(diag(invfisher).*dispersion(k));
+    switch arg.distribution
+        case 'normal'
+            mu(:,k) = eta;
+        case 'binomial'
+            mu(:,k) = 1./(1+exp(-eta));
+        case 'poisson'
+            mu(:,k) = exp(eta);
     end
 end
 tstat = b./se;
 
 if nargout > 1
 
-    % Initialize random number generator
     rng(arg.seed);
 
     % Preallocate output arrays
-    p = zeros(pnum,v);
+    p = zeros(npred,nvar);
     if nargout > 2
-        ci = zeros(2,v,pnum);
+        ci = zeros(2,nvar,npred);
     end
     if nargout > 4
-        dist = zeros(arg.nperm,v,pnum);
+        if arg.correct
+            dist = zeros(arg.nperm,npred);
+        else
+            dist = zeros(arg.nperm,nvar,npred);
+        end
     end
 
     % Iterate across predictors
-    for j = 1:pnum
+    for j = 1:npred
+
         % Define permutation target
         xred = x;
         xred(:,j) = [];
-
-        Mj = zeros(v,nobs);
-        sestar = zeros(v,1);
-        ered_all = zeros(nobs,v);
-        etared_all = zeros(nobs,v);
-
-        for k = 1:v
+        Mj = zeros(nvar,nobs);
+        sestar = zeros(nvar,1);
+        ered = zeros(nobs,nvar);
+        etared = zeros(nobs,nvar);
+        for k = 1:nvar
             switch arg.type
-                case {'freedmanlane', 'rank'}
-                    [~,Wred,etared_all(:,k),zred] = ptlocalfitglm(xred,...
+                case {'freedmanlane','rank'}
+                    [~,Wred,etared(:,k),zred] = ptlocalfitglm(xred,...
                         y(:,k),arg.distribution);
-                    ered_all(:,k) = zred-etared_all(:,k);
+                    ered(:,k) = zred-etared(:,k);
                 case 'manly'
-                    [~,Wred,~,zred] = ptlocalfitglm(ones(nobs,1),y(:,k),...
-                        arg.distribution);
-                    etared_all(:,k) = 0;
-                    ered_all(:,k) = zred;
+                    [~,Wred,~,zred] = ptlocalfitglm(ones(nobs,1),...
+                        y(:,k),arg.distribution);
+                    ered(:,k) = zred;
             end
 
             % Compute exact WLS projection matrix for fast permutation
             XW = x.*Wred;
-            inv_fisher = pinv(XW'*x);
-            Mfull = inv_fisher*XW';
+            invfisher = pinv(XW'*x);
+            Mfull = invfisher*XW';
             Mj(k,:) = Mfull(j,:);
-            sestar(k) = sqrt(inv_fisher(j,j));
+            sestar(k) = sqrt(invfisher(j,j).*dispersion(k));
+
         end
 
         % Estimate sampling distribution
-        distj = zeros(arg.nperm,v);
+        distj = zeros(arg.nperm,nvar);
         for i = 1:arg.nperm
             randidx = randperm(nobs);
-            eperm = ered_all(randidx,:);
-            zperm = etared_all+eperm;
-            bstar_j = sum(Mj'.*zperm,1);
-            distj(i,:) = bstar_j./sestar';
+            eperm = ered(randidx,:);
+            zperm = etared+eperm;
+            bstarj = sum(Mj'.*zperm,1);
+            distj(i,:) = bstarj./sestar';
         end
 
         % Apply max-correction if specified
         if arg.correct
             switch arg.tail
-                case {'both','two'}
-                    distj = max(abs(distj),[],2);
+                case 'both'
+                    refdist = max(abs(distj),[],2);
                 case 'right'
-                    distj = max(distj,[],2);
+                    refdist = max(distj,[],2);
                 case 'left'
-                    distj = min(distj,[],2);
+                    refdist = min(distj,[],2);
             end
         else
-            switch arg.tail
-                case {'both','two'}
-                    distj = abs(distj);
-            end
+            refdist = distj;
         end
 
         % Compute p-values
         switch arg.tail
-            case {'both','two'}
-                p(j,:) = (sum(abs(tstat(j,:))<=distj,1)+1)./(arg.nperm+1);
+            case 'both'
+                p(j,:) = (sum(abs(tstat(j,:))<=abs(refdist),1)+1)./...
+                    (arg.nperm+1);
             case 'right'
-                p(j,:) = (sum(tstat(j,:)<=distj,1)+1)./(arg.nperm+1);
+                p(j,:) = (sum(tstat(j,:)<=refdist,1)+1)./(arg.nperm+1);
             case 'left'
-                p(j,:) = (sum(tstat(j,:)>=distj,1)+1)./(arg.nperm+1);
+                p(j,:) = (sum(tstat(j,:)>=refdist,1)+1)./(arg.nperm+1);
         end
 
         % Compute confidence intervals
         if nargout > 2
             switch arg.tail
-                case {'both','two'}
-                    crit=prctile(distj,100*(1-arg.alpha),1);
+                case 'both'
+                    crit = prctile(abs(refdist),100*(1-arg.alpha),1);
                     ci(:,:,j) = [b(j,:)-crit.*se(j,:);b(j,:)+crit.*se(j,:)];
                 case 'right'
-                    crit=prctile(distj,100*(1-arg.alpha),1);
-                    ci(:,:,j) = [b(j,:)-crit.*se(j,:);Inf(1,v)];
+                    crit = prctile(refdist,100*(1-arg.alpha),1);
+                    ci(:,:,j) = [b(j,:)-crit.*se(j,:);Inf(1,nvar)];
                 case 'left'
-                    crit=prctile(distj,100*arg.alpha,1);
-                    ci(:,:,j) = [-Inf(1,v);b(j,:)-crit.*se(j,:)];
+                    crit = prctile(refdist,100*arg.alpha,1);
+                    ci(:,:,j) = [-Inf(1,nvar);b(j,:)-crit.*se(j,:)];
             end
         end
 
         % Store null distribution
         if nargout > 4
-            dist(:,:,j) = distj;
+            if arg.correct
+                dist(:,j) = refdist;
+            else
+                dist(:,:,j) = distj;
+            end
         end
 
     end
@@ -242,81 +254,76 @@ end
 
 % Store statistics in a structure
 if nargout > 3
-    r2 = zeros(1,v);
-    for k = 1:v
-        % Calculate squared correlation as pseudo-R2
-        c = corrcoef(y(:,k),mu_full(:,k));
-        r2(k) = c(1,2)^2;
-    end
-    stats.method = arg.type;
+    r = permucorr(y,mu);
     stats.distribution = arg.distribution;
     stats.df = df;
-    stats.se = se;
+    stats.r2 = r.^2;
     stats.tstat = tstat;
-    stats.r2 = r2;
+    stats.res = y-mu;
+    stats.se = se;
+    stats.method = arg.type;
 end
 
 end
 
-function [b, W, eta, z] = ptlocalfitglm(X, y, dist)
+function [b,W,eta,z] = ptlocalfitglm(X,y,dist)
 % Local function to perform iteratively reweighted least squares (IRLS)
-[N, P] = size(X);
+[N,P] = size(X);
 if P == 0
     b = [];
     eta = zeros(N,1);
-    if strcmpi(dist, 'binomial')
-        mu = 0.5 * ones(N,1);
-        W = 0.25 * ones(N,1);
-    elseif strcmpi(dist, 'poisson')
-        mu = mean(y) * ones(N,1);
-        W = mu; W(W < 1e-5) = 1e-5;
+    if strcmpi(dist,'binomial')
+        mu = 0.5*ones(N,1);
+        W = 0.25*ones(N,1);
+    elseif strcmpi(dist,'poisson')
+        mu = mean(y)*ones(N,1);
+        W = mu; W(W<1e-5) = 1e-5;
     else
         mu = zeros(N,1);
         W = ones(N,1);
     end
-    if strcmpi(dist, 'normal')
+    if strcmpi(dist,'normal')
         z = y;
     else
-        z = eta + (y - mu) ./ W;
+        z = eta+(y-mu)./W;
     end
     return;
 end
-if strcmpi(dist, 'normal')
-    b = pinv(X) * y;
-    eta = X * b;
-    W = ones(N, 1);
+if strcmpi(dist,'normal')
+    b = pinv(X)*y;
+    eta = X*b;
+    W = ones(N,1);
     z = y;
     return;
 end
 
 % IRLS Initialization
-if strcmpi(dist, 'binomial')
-    mu = (y + 0.5) ./ 2;
-    eta = log(mu ./ (1 - mu));
+if strcmpi(dist,'binomial')
+    mu = (y+0.5)./2;
+    eta = log(mu./(1-mu));
 else
-    mu = y + 0.1;
+    mu = y+0.1;
     eta = log(mu);
 end
-b = zeros(P, 1);
+b = zeros(P,1);
 for iter = 1:50
-    if strcmpi(dist, 'binomial')
-        W = mu .* (1 - mu);
+    if strcmpi(dist,'binomial')
+        W = mu.*(1-mu);
     else
         W = mu;
     end
     W(W < 1e-5) = 1e-5; % regularize
-    z = eta + (y - mu) ./ W;
-    XW = X .* W;
-    b_new = pinv(XW' * X) * (XW' * z);
-
-    if max(abs(b_new - b)) < 1e-6
-        b = b_new;
+    z = eta+(y-mu)./W;
+    XW = X.*W;
+    bnew = pinv(XW'*X)*(XW'*z);
+    if max(abs(bnew-b)) < 1e-6
+        b = bnew;
         break;
     end
-    b = b_new;
-    eta = X * b;
-    if strcmpi(dist, 'binomial')
-        mu = 1 ./ (1 + exp(-eta));
+    b = bnew;
+    eta = X*b;
+    if strcmpi(dist,'binomial')
+        mu = 1./(1+exp(-eta));
     else
         mu = exp(eta);
     end
